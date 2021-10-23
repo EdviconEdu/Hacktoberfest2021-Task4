6b9d1cc7bf2ea4f9187aa41d87da61f00eae1878
Kapil chaudhary <kapilchaudhary706@gmail.com>
2021-10-24 01:48:28 +0530

    Java Patch #004

--- java/app/src/main/java/com/nimesh/foodsapp/api/FoodClient.java
+++ java/app/src/main/java/com/nimesh/foodsapp/api/FoodClient.java
@@ -17,9 +17,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class FoodClient {
 	
 	/* Bug Start #004 */
-    private static final String BASE_URL == "https://www.themealdb.com/api/json/v1/1/";
+    private static final String BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 
-    public static Retrofit getFoodClient( {
+    public static Retrofit getFoodClient() {
         return new Retrofit.Builder().baseUrl(BASE_URL)
                 .client(provideOkHttp())
                 .addConverterFactory(GsonConverterFactory.create())
